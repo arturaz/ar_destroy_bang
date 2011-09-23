9c6d9ddac808742b880abc39963e40b3674ddc76
Henrikas Elsbergas <netyciuks@gmail.com>
2011-09-23 17:06:35 +0300

    Rails support.

--- init.rb
+++ init.rb
@@ -1,14 +1,12 @@
-module ActiveRecord
-  # Raised by #destroy! if record was not destroyed.
-  class RecordNotDestroyed < ActiveRecord::ActiveRecordError; end
-  
-  module Persistence
-    # Destroys record, raises error if not destroyed.
-    def destroy!
-      return_value = destroy
-      raise ActiveRecord::RecordNotDestroyed.
-        new("#{self} was not destroyed!") unless destroyed?
-      return_value
-    end
+# Raised by #destroy! if record was not destroyed.
+class ActiveRecord::RecordNotDestroyed < ActiveRecord::ActiveRecordError; end
+
+module ActiveRecord::Persistence
+  # Destroys record, raises error if not destroyed.
+  def destroy!
+    return_value = destroy
+    raise ActiveRecord::RecordNotDestroyed.
+      new("#{self} was not destroyed!") unless destroyed?
+    return_value
   end
 end
